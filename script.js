02ec5248e9f4bb90dbe19aa10353f3ffc8c720c5
Manya Kalra <kalramanya212@gmail.com>
2025-01-03 11:23:38 +0530

    Fix typo

--- script.js
+++ script.js
@@ -300,3 +300,4 @@ document.addEventListener('DOMContentLoaded', function () {
 <!-- Update 2024-12-25T13:31:36+05:30 -->
 <!-- Update 2024-12-25T07:21:36+05:30 -->
 <!-- Update 2025-01-03T15:14:38+05:30 -->
+<!-- Update 2025-01-03T11:23:38+05:30 -->
