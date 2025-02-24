c4e97b4209c8e725343aed07db27503c0cfe582e
Manya Kalra <kalramanya212@gmail.com>
2025-02-24 08:30:11 +0530

    Improve performance

--- script.js
+++ script.js
@@ -309,3 +309,4 @@ document.addEventListener('DOMContentLoaded', function () {
 <!-- Update 2025-01-28T11:42:58+05:30 -->
 <!-- Update 2025-02-10T09:49:06+05:30 -->
 <!-- Update 2025-02-10T11:15:06+05:30 -->
+<!-- Update 2025-02-24T08:30:11+05:30 -->
