fd52ed0d19d47bacaafc01a509004f2cb64b7bf3
Manya Kalra <kalramanya212@gmail.com>
2025-05-15 15:39:50 +0530

    Update dependencies

--- script.js
+++ script.js
@@ -314,3 +314,4 @@ document.addEventListener('DOMContentLoaded', function () {
 <!-- Update 2025-04-14T10:17:41+05:30 -->
 <!-- Update 2025-04-18T09:25:46+05:30 -->
 <!-- Update 2025-05-14T15:47:49+05:30 -->
+<!-- Update 2025-05-15T15:39:50+05:30 -->
